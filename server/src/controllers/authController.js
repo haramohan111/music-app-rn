101d3248dd1153f1f75a885fc2dfbcc362dae1e0
haramohan111 <haramohan111@gmail.com>
2025-08-21 22:55:36 +0530

    remove domain

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -57,7 +57,7 @@ exports.login = async (req, res, next) => {
       sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       maxAge:  24 * 60 * 60 * 1000, // 1 day,
       path: '/',
-      ...(process.env.NODE_ENV === 'production' && { domain: '.vercel.app' }),
+   
     });
 
     res.cookie('refreshToken', refreshToken, {
@@ -66,7 +66,7 @@ exports.login = async (req, res, next) => {
       sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       maxAge: 7 * 24 * 60 * 1000,
       path: '/', // 7 days
-      ...(process.env.NODE_ENV === 'production' && { domain: '.vercel.app' }),
+      
     });
 
     res.status(200).json({
