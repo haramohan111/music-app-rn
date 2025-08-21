104d709e9ba8aa68ca90e3644a5e20e96fafe56b
haramohan111 <haramohan111@gmail.com>
2025-08-21 22:43:45 +0530

    max agee now 1 day previos 1 minute

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -55,7 +55,7 @@ exports.login = async (req, res, next) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
-      maxAge: 60 * 1000, // 1 minutes,
+      maxAge:  24 * 60 * 60 * 1000, // 1 day,
       path: '/',
       ...(process.env.NODE_ENV === 'production' && { domain: '.vercel.app' }),
     });
