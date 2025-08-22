e165015c6299f662c8c06f167e8914a3da3208f6
haramohan111 <haramohan111@gmail.com>
2025-08-22 22:46:13 +0530

    vite change

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-
+//https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
-  },
-});
+})
+
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       define: {
+//         global: 'globalThis',
+//       },
+//       plugins: [
+//         NodeGlobalsPolyfillPlugin({
+//           buffer: true,
+//         }),
+//       ],
+//     },
+//   },
+// });
 
 
